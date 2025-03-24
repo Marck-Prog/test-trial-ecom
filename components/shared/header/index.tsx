9109f293932e62296bd29555f95a7174349487a3
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-25 00:14:46 +0800

    Refactor Header component to utilize Navbar and implement dropdown items for better organization and structure.

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,59 +1,37 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { getAllCategories } from '@/lib/actions/product.actions'
-import Menu from './menu'
-import Search from './search'
-import data from '@/lib/data'
-import Sidebar from './sidebar'
 import { getSetting } from '@/lib/actions/setting.actions'
-import { getTranslations } from 'next-intl/server'
+import Navbar from './navbar'
 
 export default async function Header() {
-  const categories = await getAllCategories()
-  const { site } = await getSetting()
-  const t = await getTranslations()
-  return (
-    <header className='bg-black  text-white'>
-      <div className='px-2'>
-        <div className='flex items-center justify-between'>
-          <div className='flex items-center'>
-            <Link
-              href='/'
-              className='flex items-center header-button font-extrabold text-2xl m-1 '
-            >
-              <Image
-                src={site.logo}
-                width={40}
-                height={40}
-                alt={`${site.name} logo`}
-              />
-              {site.name}
-            </Link>
-          </div>
+  // Data for dropdowns (can be fetched from an API or CMS in a real app)
+  const dropdownItems = {
+    threads: [
+      {
+        category: 'SHIRTS',
+        items: [
+          'Classic Tees',
+          'Oversized Tees',
+          'Wash Tees',
+          'Kids Tees',
+          'Baby Crop',
+          'Tanks',
+          'Crop Tees',
+          'Long Sleeves',
+        ],
+      },
+      { category: 'SWEATS', items: ['Hoodies', 'Sweatshirts', 'Tracksuit'] },
+    ],
+    accessories: [],
+    collabs: [],
+    themes: [],
+    custom: [],
+    whatsNew: [],
+  }
 
-          <div className='hidden md:block flex-1 max-w-xl'>
-            <Search />
-          </div>
-          <Menu />
-        </div>
-        <div className='md:hidden block py-2'>
-          <Search />
-        </div>
-      </div>
-      <div className='flex items-center px-3 mb-[1px]  bg-gray-800'>
-        <Sidebar categories={categories} />
-        <div className='flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]'>
-          {data.headerMenus.map((menu) => (
-            <Link
-              href={menu.href}
-              key={menu.href}
-              className='header-button !p-2 '
-            >
-              {t('Header.' + menu.name)}
-            </Link>
-          ))}
-        </div>
-      </div>
+  const { site } = await getSetting()
+
+  return (
+    <header className='sticky top-0 z-50'>
+      <Navbar site={site} dropdownItems={dropdownItems} />
     </header>
   )
 }
