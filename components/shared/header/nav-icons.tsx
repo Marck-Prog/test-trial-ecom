a065eb76689d9786f8d766ad1920d47b9f1608ee
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-25 20:10:02 +0800

    Refactor NavIcons component to improve layout and structure by consolidating button elements, enhancing mobile responsiveness, and integrating UserButtonClient for better user interaction.

--- components/shared/header/nav-icons.tsx
+++ components/shared/header/nav-icons.tsx
@@ -5,43 +5,51 @@ import { Button } from '@/components/ui/button'
 import { ShoppingCart, UserRound } from 'lucide-react'
 import LanguageSwitcher from './language-switcher'
 import { Search } from 'lucide-react'
+import UserButtonClient from './UserButtonClient'
 
 const NavIcons = () => {
   return (
     <>
-      {/* Language switcher */}
-      {/* <Button
-        variant='ghost'
-        size='icon'
-        className='focus:outline-none ml-5 hidden lg:flex hover:bg-white'
-      > */}
-      <LanguageSwitcher />
-      {/* </Button> */}
-      {/* Search */}
-      <Button
+      <div className='flex items-center gap-2'>
+        <LanguageSwitcher />
+        <Button
+          variant='ghost'
+          size='icon'
+          className='focus:outline-none hidden lg:flex '
+        >
+          <Search className='h-5 w-5' />
+        </Button>
+        <UserButtonClient />
+
+        {/* <LanguageSwitcher /> */}
+        {/* Search */}
+        {/* <Button
         variant='ghost'
         size='icon'
         className='focus:outline-none hidden lg:flex ml-2 hover:bg-white'
       >
         <Search className='h-5 w-5' />
-      </Button>
-      <Button
+      </Button> */}
+        {/* User */}
+        {/* <Button
         variant='ghost'
         size='icon'
         className='flex focus:outline-none items-center justify-center w-10 h-10 relative hover:bg-white'
       >
         <UserRound className='h-5 w-5' />
-      </Button>
-      <Button
-        variant='ghost'
-        size='icon'
-        className='flex focus:outline-none items-center justify-center w-10 h-10 relative hover:bg-white'
-      >
-        <ShoppingCart className='h-5 w-5' />
-        <Badge className='absolute -top-0.5 left-5 h-5 w-5 flex items-center justify-center p-0 rounded-full'>
-          6
-        </Badge>
-      </Button>
+      </Button> */}
+        {/* Shopping cart */}
+        <Button
+          variant='ghost'
+          size='icon'
+          className='flex focus:outline-none items-center justify-center w-10 h-10 relative hover:bg-white'
+        >
+          <ShoppingCart className='h-5 w-5' />
+          <Badge className='absolute -top-0.5 left-5 h-5 w-5 flex items-center justify-center p-0 rounded-full'>
+            6
+          </Badge>
+        </Button>
+      </div>
     </>
   )
 }
