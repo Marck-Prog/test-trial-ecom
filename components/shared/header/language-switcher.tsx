e27bbc456a4daa2d5b47b28ee50612ecf8bcda56
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-25 20:09:38 +0800

    Refactor LanguageSwitcher component to improve dropdown functionality and UI, replacing icon with MUI's KeyboardArrowDownIcon, enhancing currency and language selection, and cleaning up unused code for better readability.

--- components/shared/header/language-switcher.tsx
+++ components/shared/header/language-switcher.tsx
@@ -16,13 +16,14 @@ import { i18n } from '@/i18n-config'
 import { Link, usePathname } from '@/i18n/routing'
 import { setCurrencyOnServer } from '@/lib/actions/setting.actions'
 import { ChevronDownIcon } from 'lucide-react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import Image from 'next/image'
 
 export default function LanguageSwitcher() {
-  const [isRotated, setIsRotated] = useState(false)
-  const { locales } = i18n
+  const [open, setOpen] = useState(false)
   const locale = useLocale()
   const pathname = usePathname()
+  const { locales } = i18n
 
   const {
     setting: { availableCurrencies, currency },
@@ -34,19 +35,9 @@ export default function LanguageSwitcher() {
     setCurrency(newCurrency)
   }
 
-  const handleClick = () => {
-    setIsRotated(!isRotated)
-  }
-
-  // Get the currently selected currency object with a fallback
   const currentCurrency =
     availableCurrencies.find((c) => c.code === currency) ||
     availableCurrencies[0]
-
-  // Debug missing flag
-  if (!currentCurrency?.flag || currentCurrency.flag === '') {
-    console.warn('Missing or empty flag for currency:', currentCurrency)
-  }
 
   return (
     <>
@@ -61,43 +52,32 @@ export default function LanguageSwitcher() {
             className=''
           />
         ) : (
-          <span>🏳️</span> // Fallback emoji if flag is missing
+          <span>🏳️</span>
         )}
         <span className='text-black font-bold mr-2'>
           {currentCurrency.symbol}
         </span>
         <span className='text-black font-bold'>{currentCurrency.code}</span>
       </div>
-      <div className='ml-5 hidden lg:flex hover:bg-white'>
-        <DropdownMenu>
+      <div className='ml-3 hidden lg:flex hover:bg-white'>
+        <DropdownMenu open={open} onOpenChange={setOpen}>
           <DropdownMenuTrigger className='focus:outline-none'>
             <div className='flex items-center gap-1 mb-1'>
               <span className='text-xl'>
                 {locales.find((l) => l.code === locale)?.icon}
               </span>
-              <ChevronDownIcon
-                className={`transition-transform duration-300 ${isRotated ? 'rotate-180' : ''} mt-1`}
-                style={{ width: '18px', height: '18px' }}
-                onClick={handleClick}
+              <KeyboardArrowDownIcon
+                sx={{
+                  color: 'black',
+                  mt: 0.5,
+                  fontSize: '1rem',
+                  transform: open ? 'rotate(180deg)' : 'rotate(0deg)',
+                  transition: 'transform 0.3s ease-in-out',
+                }}
               />
             </div>
           </DropdownMenuTrigger>
-          <DropdownMenuContent className='w-56 mt-[5px] rounded-none border-none'>
-            <DropdownMenuSeparator />
-            <DropdownMenuLabel>Language</DropdownMenuLabel>
-            <DropdownMenuRadioGroup value={locale}>
-              {locales.map((c) => (
-                <DropdownMenuRadioItem key={c.name} value={c.code}>
-                  <Link
-                    className='w-full flex items-center gap-1'
-                    href={pathname}
-                    locale={c.code}
-                  >
-                    <span className='text-lg'>{c.icon}</span> {c.name}
-                  </Link>
-                </DropdownMenuRadioItem>
-              ))}
-            </DropdownMenuRadioGroup>
+          <DropdownMenuContent className='w-56 mt-1 border-t border-none rounded-none'>
             <DropdownMenuSeparator />
             <DropdownMenuLabel>Currency</DropdownMenuLabel>
             <DropdownMenuRadioGroup
@@ -106,7 +86,7 @@ export default function LanguageSwitcher() {
             >
               {availableCurrencies.map((c) => (
                 <DropdownMenuRadioItem key={c.name} value={c.code}>
-                  <div className='flex items-center gap-2'>
+                  <div className='flex items-center gap-2 cursor-pointer'>
                     {c.flag && c.flag !== '' ? (
                       <Image
                         src={c.flag}
@@ -115,12 +95,28 @@ export default function LanguageSwitcher() {
                         height={20}
                       />
                     ) : (
-                      <span>🏳️</span> // Fallback emoji
+                      <span>🏳️</span>
                     )}
                     <span>
                       {c.symbol} {c.code}
                     </span>
                   </div>
+                </DropdownMenuRadioItem>
+              ))}
+            </DropdownMenuRadioGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuLabel>Language</DropdownMenuLabel>
+            <DropdownMenuRadioGroup value={locale}>
+              {locales.map((c) => (
+                <DropdownMenuRadioItem key={c.name} value={c.code}>
+                  <Link
+                    className='w-full flex items-center gap-1 cursor-pointer'
+                    href={pathname}
+                    locale={c.code}
+                  >
+                    {c.name}
+                    <span className='text-lg'>{c.icon}</span>
+                  </Link>
                 </DropdownMenuRadioItem>
               ))}
             </DropdownMenuRadioGroup>
