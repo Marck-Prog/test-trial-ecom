e99eaf4df6cc088c80254a3d863658c23093ca62
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-25 12:59:12 +0800

    Refactor NavIcons component to integrate LanguageSwitcher, streamline icon usage, and enhance layout for improved user experience.

--- components/shared/header/nav-icons.tsx
+++ components/shared/header/nav-icons.tsx
@@ -1,26 +1,23 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
-import { Search, UserRound, ShoppingCart, Heart } from 'lucide-react'
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { ShoppingCart, UserRound } from 'lucide-react'
+import LanguageSwitcher from './language-switcher'
+import { Search } from 'lucide-react'
 
 const NavIcons = () => {
   return (
     <>
-      {/* Currency/Language - Hidden on small screens */}
-      <div className='hidden xl:flex items-center space-x-1'>
-        <Image
-          src='/images/philippine-flag.png'
-          alt='Philippine Flag'
-          width={19}
-          height={20}
-          className=''
-        />
-        <span className='text-black font-bold'>PHP ₱</span>
-      </div>
-
-      {/* Icons */}
+      {/* Language switcher */}
+      {/* <Button
+        variant='ghost'
+        size='icon'
+        className='focus:outline-none ml-5 hidden lg:flex hover:bg-white'
+      > */}
+      <LanguageSwitcher />
+      {/* </Button> */}
+      {/* Search */}
       <Button
         variant='ghost'
         size='icon'
@@ -31,19 +28,9 @@ const NavIcons = () => {
       <Button
         variant='ghost'
         size='icon'
-        className='focus:outline-none hidden lg:flex hover:bg-white'
-      >
-        <UserRound className='h-5 w-5' />
-      </Button>
-      <Button
-        variant='ghost'
-        size='icon'
         className='flex focus:outline-none items-center justify-center w-10 h-10 relative hover:bg-white'
       >
-        <Heart className='h-5 w-5' />
-        <Badge className='absolute -top-0.5 left-5 h-5 w-5 flex items-center justify-center p-0 rounded-full'>
-          3
-        </Badge>
+        <UserRound className='h-5 w-5' />
       </Button>
       <Button
         variant='ghost'
