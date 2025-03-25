6e1902ed6cb70a14c0b08304e7c9c56cf81e1f98
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-25 20:07:56 +0800

    Add remote image patterns for Google and Cloudinary to next.config.ts, enhancing image support for external resources.

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,24 @@ import type { NextConfig } from 'next'
 import withNextIntl from 'next-intl/plugin'
 
 const nextConfig: NextConfig = withNextIntl()({
-  /* config options here */
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'utfs.io',
         port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com', // Example: Cloudinary for product images
+        port: '',
+        pathname: '/**',
       },
     ],
   },
