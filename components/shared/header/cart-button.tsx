c3c222afc1e7a01430fd8a035113b53d70e39eb8
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-26 18:50:40 +0800

    Refactor CartButton component to enhance functionality and UI; replace Link with Button for sidebar toggle, update cart item count display, and improve styling for better user experience.

--- components/shared/header/cart-button.tsx
+++ components/shared/header/cart-button.tsx
@@ -1,53 +1,46 @@
 'use client'
 
-import { ShoppingCartIcon } from 'lucide-react'
 import Link from 'next/link'
 import useIsMounted from '@/hooks/use-is-mounted'
-import useShowSidebar from '@/hooks/use-cart-sidebar'
 import { cn } from '@/lib/utils'
 import useCartStore from '@/hooks/use-cart-store'
 import { useLocale, useTranslations } from 'next-intl'
 import { getDirection } from '@/i18n-config'
+import { Button } from '@/components/ui/button'
+import { ShoppingCart } from 'lucide-react'
 
 export default function CartButton() {
   const isMounted = useIsMounted()
   const {
     cart: { items },
+    toggleSidebar,
   } = useCartStore()
   const cartItemsCount = items.reduce((a, c) => a + c.quantity, 0)
-  const showSidebar = useShowSidebar()
   const t = useTranslations()
-
   const locale = useLocale()
-  return (
-    <Link href='/cart' className='px-1 header-button'>
-      <div className='flex items-end text-xs relative'>
-        <ShoppingCartIcon className='h-8 w-8' />
 
+  return (
+    <div className='relative'>
+      <Button
+        variant='ghost'
+        size='icon'
+        className='flex focus:outline-none items-center justify-center w-10 h-10 relative hover:bg-white'
+        onClick={() => toggleSidebar(true)}
+      >
+        <ShoppingCart className='h-5 w-5' />
         {isMounted && (
           <span
             className={cn(
-              `bg-black  px-1 rounded-full text-primary text-base font-bold absolute ${
-                getDirection(locale) === 'rtl' ? 'right-[5px]' : 'left-[10px]'
+              `bg-[#22b92f] px-2 py-0.5 rounded-full text-white absolute ${
+                getDirection(locale) === 'rtl' ? 'right-[5px]' : 'left-[20px]'
               } top-[-4px] z-10`,
-              cartItemsCount >= 10 && 'text-sm px-0 p-[1px]'
+              cartItemsCount >= 10 && 'text-sm px-1 py-0.5'
             )}
           >
             {cartItemsCount}
           </span>
         )}
-        <span className='font-bold'>{t('Header.Cart')}</span>
-
-        {showSidebar && (
-          <div
-            className={`absolute top-[20px] ${
-              getDirection(locale) === 'rtl'
-                ? 'left-[-16px] rotate-[-270deg]'
-                : 'right-[-16px] rotate-[-90deg]'
-            }  z-10   w-0 h-0 border-l-[7px] border-r-[7px] border-b-[8px] border-transparent border-b-background`}
-          ></div>
-        )}
-      </div>
-    </Link>
+      </Button>
+    </div>
   )
 }
