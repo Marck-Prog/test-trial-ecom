864f485ca4016d8ea68e0ad1833788b8857cf415
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-27 20:00:05 +0800

    Refactor HomeCarousel component to enhance UI and code clarity by updating layout structure, improving button styles, and adding loop functionality to the carousel. Removed unused imports and optimized the overall design for better user experience.

--- components/shared/home/home-carousel.tsx
+++ components/shared/home/home-carousel.tsx
@@ -12,7 +12,6 @@ import {
 } from '@/components/ui/carousel'
 import Link from 'next/link'
 import { cn } from '@/lib/utils'
-import { Button } from '@/components/ui/button'
 import { useTranslations } from 'next-intl'
 import { ICarousel } from '@/types'
 
@@ -20,48 +19,63 @@ export function HomeCarousel({ items }: { items: ICarousel[] }) {
   const plugin = React.useRef(
     Autoplay({ delay: 3000, stopOnInteraction: true })
   )
-
   const t = useTranslations('Home')
 
   return (
-    <Carousel
-      dir='ltr'
-      plugins={[plugin.current]}
-      className='w-full mx-auto '
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
-    >
-      <CarouselContent>
-        {items.map((item) => (
-          <CarouselItem key={item.title}>
-            <Link href={item.url}>
-              <div className='relative w-full h-[100vh] -m-1'>
-                <Image
-                  src={item.image}
-                  alt={item.title}
-                  fill
-                  className='object-cover w-full h-full'
-                  priority
-                />
-                <div className='absolute w-full md:w-1/3 left-4 md:left-32 top-1/2 transform -translate-y-1/2 px-6'>
-                  <h2
-                    className={cn(
-                      'text-xl md:text-6xl font-bold mb-4 text-primary'
-                    )}
-                  >
-                    {t(`${item.title}`)}
-                  </h2>
-                  <Button className='hidden md:block'>
-                    {t(`${item.buttonCaption}`)}
-                  </Button>
+    <div className='relative group'>
+      <Carousel
+        dir='ltr'
+        plugins={[plugin.current]}
+        className='w-full mx-auto'
+        onMouseEnter={plugin.current.stop}
+        onMouseLeave={plugin.current.reset}
+        opts={{
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {items.map((item) => (
+            <CarouselItem key={item.title}>
+              <Link href={item.url}>
+                <div className='flex aspect-[16/6] items-center justify-center p-6 relative -m-1'>
+                  <Image
+                    src={item.image}
+                    alt={item.title}
+                    fill
+                    className='object-cover'
+                    priority
+                  />
+                  <div className='absolute w-1/3 left-16 md:left-32 top-1/2 transform -translate-y-1/2'>
+                    <h2
+                      className={cn(
+                        'text-xl md:text-6xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-blue-800'
+                      )}
+                    >
+                      {t(`${item.title}`)}
+                    </h2>
+                    <div className='flex justify-center w-24 bg-gray-200 py-2 hover:bg-transparent duration-300 rounded-md relative group/button'>
+                      <button
+                        type='button'
+                        className='hidden md:block duration-500 group-hover/button:tracking-widest cursor-pointer relative after:content-[""] after:absolute after:bottom-0 after:left-1/2 after:w-0 after:h-[2px] after:bg-current after:transition-all after:duration-300 group-hover/button:after:w-full group-hover/button:after:left-0'
+                      >
+                        {t(`${item.buttonCaption}`)}
+                      </button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious className='left-0 md:left-12' />
-      <CarouselNext className='right-0 md:right-12' />
-    </Carousel>
+              </Link>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious
+          className='left-0 md:left-0 -translate-x-full group-hover:translate-x-10 
+          transition-transform duration-300 opacity-0 group-hover:opacity-100'
+        />
+        <CarouselNext
+          className='right-0 md:right-0 translate-x-full group-hover:-translate-x-10 
+          transition-transform duration-300 opacity-0 group-hover:opacity-100'
+        />
+      </Carousel>
+    </div>
   )
 }
